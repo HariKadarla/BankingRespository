9d5a297c3ef3fb710a7f16564dd3410fe7e2273f
Hari Kishore <harikishorekadarla@gmail.com>
2021-09-17 12:34:11 +0530

    Initial Commit Version

---
+++ BankingLibrary/CalcInterest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankingLibrary
+{
+    public class CalcInterest : ICalcInterest
+    {
+        public int Calculate(int Principle, int RateOfInterest, int Years)
+        {
+            int SimpleInt = Principle * RateOfInterest * Years / 100;
+            return SimpleInt;
+        }
+    }
+}
